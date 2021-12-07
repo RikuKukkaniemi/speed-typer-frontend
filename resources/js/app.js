ef2ebec1a8158fc68aa4250e3384ea83daae101d
Riku <kukkaniemi.riku@gmail.com>
2021-12-07 21:40:42 +0200

    Add Vue

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,9 @@
 require('./bootstrap');
+
+window.Vue = require('vue').default;
+
+Vue.component('game-layout-component', require('./components/GameLayoutComponent.vue').default);
+
+const app = new Vue({
+    el: '#app',
+});
---
+++ resources/js/components/GameLayoutComponent.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="container">
+      <h1>Speed Typer</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      highscores: {},
+    };
+  }
+};
+</script>
