0884f81148849aec85317a9d5dc7ad69aa7469e6
Riku <kukkaniemi.riku@gmail.com>
2022-01-04 20:37:30 +0200

    Add MainMenuComponent

--- resources/js/components/GameLayoutComponent.vue
+++ resources/js/components/GameLayoutComponent.vue
@@ -4,53 +4,7 @@
       <h1 class="text-center m-5">Speed Typer</h1>
     </div>
     <div v-if="!gameRunning">
-      <div class="row justify-content-center mb-4">
-        <p class="text-center col-auto col-lg-5">
-            Type the words you as precisely as you can before timer runs out. The points are determined on how precisely you type the words.
-        </p>
-        <p class="text-center">Good luck!</p>
-      </div>
-
-      <div class="row justify-content-center align-items-center">
-        <div class="row justify-content-center">
-          <p class="col-auto text-center">Select the language of words:</p>
-        </div>
-        <form class="mt-1" @submit.prevent="startGame">
-          <div class="row justify-content-center align-items-center">
-            <div class="col-auto">
-              <div class="form-check">
-                <input
-                  class="form-check-input"
-                  type="radio"
-                  name="flexRadioDefault"
-                  id="radioEn"
-                  value="en"
-                  v-model="wordLanguage"
-                  checked
-                />
-                <label class="form-check-label" for="radioEn"> English </label>
-              </div>
-            </div>
-            <div class="col-auto">
-              <div class="form-check">
-                <input
-                  class="form-check-input"
-                  type="radio"
-                  name="flexRadioDefault"
-                  id="radioFi"
-                  value="fi"
-                  v-model="wordLanguage"
-                />
-                <label class="form-check-label" for="radioFi"> Finnish </label>
-              </div>
-            </div>
-          </div>
-
-          <div class="row justify-content-center align-items-center">
-            <input class="col-auto m-3 bg-light" type="submit" value="Start Game" />
-          </div>
-        </form>
-      </div>
+      <main-menu-component @chooseLanguage="startGame" />
     </div>
     <div
       class="row justify-content-center align-items-center"
@@ -170,9 +124,14 @@
 </template>
 
 <script>
+import MainMenuComponent from './MainMenuComponent.vue'
+
 import moment from "moment";
 
 export default {
+  components: {
+    MainMenuComponent
+  },
   data() {
     return {
       gameRunning: false,
@@ -192,9 +151,9 @@ export default {
     };
   },
   methods: {
-    startGame() {
+    startGame(wordLanguage) {
       this.words = [];
-      axios.get("/words/" + this.wordLanguage).then((response) => {
+      axios.get("/words/" + wordLanguage).then((response) => {
         this.words = response.data.words;
         this.currentWordIndex = 0;
         this.userInput = "";
---
+++ resources/js/components/MainMenuComponent.vue
@@ -0,0 +1,66 @@
+<template>
+  <div>
+    <div class="row justify-content-center mb-4">
+      <p class="text-center col-auto col-lg-5">
+        Type the words you as precisely as you can before timer runs out. The
+        points are determined on how precisely you type the words.
+      </p>
+      <p class="text-center">Good luck!</p>
+    </div>
+
+    <div class="row justify-content-center align-items-center">
+      <div class="row justify-content-center">
+        <p class="col-auto text-center">Select the language of words:</p>
+      </div>
+      <form class="mt-1" @submit.prevent="$emit('chooseLanguage', wordLanguage)">
+        <div class="row justify-content-center align-items-center">
+          <div class="col-auto">
+            <div class="form-check">
+              <input
+                class="form-check-input"
+                type="radio"
+                name="flexRadioDefault"
+                id="radioEn"
+                value="en"
+                v-model="wordLanguage"
+                checked
+              />
+              <label class="form-check-label" for="radioEn"> English </label>
+            </div>
+          </div>
+          <div class="col-auto">
+            <div class="form-check">
+              <input
+                class="form-check-input"
+                type="radio"
+                name="flexRadioDefault"
+                id="radioFi"
+                value="fi"
+                v-model="wordLanguage"
+              />
+              <label class="form-check-label" for="radioFi"> Finnish </label>
+            </div>
+          </div>
+        </div>
+
+        <div class="row justify-content-center align-items-center">
+          <input
+            class="col-auto m-3 bg-light"
+            type="submit"
+            value="Start Game"
+          />
+        </div>
+      </form>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+    data() {
+    return {
+      wordLanguage: "en",
+    };
+  },
+}
+</script>
